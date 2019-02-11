94be6a771ee699d54ff64cccd98c536c26d665b6
Heng Li <lh3@me.com>
2019-02-11 10:16:56 -0500

    added chaining

--- mgpriv.h
+++ mgpriv.h
@@ -5,11 +5,16 @@
 #include "kalloc.h"
 #include "minigraph.h"
 
+#define MG_SEED_SEG_SHIFT  48
+#define MG_SEED_SEG_MASK   (0xffULL<<(MG_SEED_SEG_SHIFT))
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void mg_sketch(void *km, const char *str, int len, int w, int k, uint32_t rid, int is_hpc, mg128_v *p);
+
+mg128_t *mg_chain_dp(int max_dist_x, int max_dist_y, int bw, int max_skip, int min_cnt, int min_sc, int is_cdna, int n_segs, int64_t n, mg128_t *a, int *n_u_, uint64_t **_u, void *km);
 
 void radix_sort_128x(mg128_t *beg, mg128_t *end);
 void radix_sort_64(uint64_t *beg, uint64_t *end);
