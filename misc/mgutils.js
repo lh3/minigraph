d4809a4f3b66a282464a7f94733dba67e543701a
Heng Li <lh3@me.com>
2019-09-18 12:59:38 -0400

    added subgaf to mgutils.js

--- misc/mgutils.js
+++ misc/mgutils.js
@@ -73,6 +73,54 @@ function mg_cmd_renamefa(args)
 	buf.destroy();
 }
 
+function mg_cmd_subgaf(args)
+{
+	if (args.length < 2) {
+		print("Usage: mgutils.js subgaf <in.gaf> <reg>");
+		exit(1);
+	}
+
+	var m, ctg, st, en;
+	if ((m = /^(\S+):(\S+)-(\S+)/.exec(args[1])) != null)
+		ctg = m[1], st = parseInt(m[2]), en = parseInt(m[3]);
+
+	var buf = new Bytes();
+	var file = new File(args[0]);
+	var re = /([><])([^\s><]+):(\d+)-(\d+)/g;
+
+	while (file.readline(buf) >= 0) {
+		var t = buf.toString().split("\t");
+		var l = parseInt(t[6]), s = parseInt(t[7]), e = parseInt(t[8]);
+		var regs = [];
+		if (t[5][0] == '>' || t[5][0] == '<') {
+			var m, x = 0;
+			//print(buf);
+			while ((m = re.exec(t[5])) != null) {
+				var a = parseInt(m[3]), b = parseInt(m[4]), c = b - a;
+				if (x == 0) {
+					if (b - a <= s) throw Error("Inconsistent!");
+					a += s;
+				}
+				if (x + c == l) b -= l - e;
+				//print(m[2], a, b);
+				regs.push([m[2], a, b]);
+				x += c;
+			}
+		} else {
+			regs.push([t[5], s, e]);
+		}
+		var hit = false;
+		for (var i = 0; i < regs.length; ++i) {
+			if (regs[i][0] == ctg && regs[i][2] > st && en > regs[i][1])
+				hit = true;
+		}
+		if (hit) print(buf);
+	}
+
+	file.close();
+	buf.destroy();
+}
+
 /*************************
  ***** main function *****
  *************************/
@@ -83,11 +131,13 @@ function main(args)
 		print("Usage: mgutils.js <command> [arguments]");
 		print("Commands:");
 		print("  renamefa     add a prefix to sequence names in FASTA");
+		print("  subgaf       extract GAF overlapping with a region");
 		exit(1);
 	}
 
 	var cmd = args.shift();
 	if (cmd == 'renamefa') mg_cmd_renamefa(args);
+	else if (cmd = 'subgaf') mg_cmd_subgaf(args);
 	else throw Error("unrecognized command: " + cmd);
 }
 
