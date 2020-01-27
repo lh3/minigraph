42751f6c235168f50d993c329fcb8967dd1305ad
Heng Li <lh3@me.com>
2020-01-26 23:55:58 -0500

    r353: fixed missing repeat INDELs

--- ggsimple.c
+++ ggsimple.c
@@ -134,8 +134,14 @@ void mg_ggsimple(void *km, const mg_ggopt_t *opt, gfa_t *g, int32_t n_seq, const
 				} else pd = (int32_t)p->x - (int32_t)q->x;
 				if ((opt->flag&MG_G_NO_QOVLP) && (int32_t)p->y < far_q) s = 1; // query overlap
 				else if (pd == qd && c == 0) s = -opt->match_pen;
-				else if (pd > qd) s = (int32_t)(c + (pd - qd) * a_dens + .499);
-				else s = c;
+				else if (pd > qd) {
+					double x = qd * a_dens;
+					x = x > c? x : c;
+					s = (int32_t)(x + (pd - qd) * a_dens + .499);
+				} else {
+					s = (int32_t)(qd * a_dens + .499);
+					s = s > c? s : c;
+				}
 				sc[j - 1] = s;
 				meta[j-1] = (uint64_t)pd<<32 | off_l0;
 			}
